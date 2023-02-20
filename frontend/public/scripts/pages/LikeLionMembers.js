9f8982cc111894d4ba96738711f713f370cea6b0
nahee09 <tink159976@gmail.com>
2023-02-20 15:53:46 +0900

    cra사용

--- frontend/public/scripts/pages/LikeLionMembers.js
+++ frontend/public/scripts/pages/LikeLionMembers.js
@@ -12,8 +12,8 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
 function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
-import { likeLionMembers } from '../data/likeLionMembers.js';
-var LikeLionMembers = /*#__PURE__*/function (_React$Component) {
+import { likeLionMembers } from "../data/likeLionMembers.js";
+export var LikeLionMembers = /*#__PURE__*/function (_React$Component) {
   _inherits(LikeLionMembers, _React$Component);
   var _super = _createSuper(LikeLionMembers);
   function LikeLionMembers() {
@@ -40,20 +40,11 @@ var LikeLionMembers = /*#__PURE__*/function (_React$Component) {
   _createClass(LikeLionMembers, [{
     key: "calcLabCount",
     value: function calcLabCount() {
-      // 데이터 분석
-      // 내가 무얼을 해야 하나?
-      // - 105개의 데이터를 순회해서 lab의 갯수가 몇 개인지를 확인해야 한다.
-
       var labSet = new Set();
       this.state.members.forEach(function (_ref) {
         var lab = _ref.lab;
         return labSet.add(lab);
       });
-
-      // 그걸 하려면 어떤 로직을 짜야 하나?
-      // - 배열??? 아니면 다른 데이터를?? 🤔
-      // 그러면 결과 값은 무엇을 내보내야 하나?
-      // - 랩의 갯수
       return labSet.size;
     }
   }, {
@@ -64,7 +55,7 @@ var LikeLionMembers = /*#__PURE__*/function (_React$Component) {
       return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, "\uBA4B\uC7C1\uC774 \uC0AC\uC790\uCC98\uB7FC \uD504\uB860\uD2B8\uC5D4\uB4DC \uC2A4\uCFE8 4\uAE30 \uBA64\uBC84"), /*#__PURE__*/React.createElement("div", {
         role: "group",
         style: {
-          display: 'flex',
+          display: "flex",
           gap: 8
         }
       }, Array(this.labCount).fill().map(function (_, index /* 0, 1, 2, ..., 10 */) {
@@ -82,7 +73,7 @@ var LikeLionMembers = /*#__PURE__*/function (_React$Component) {
           gender = _ref2.gender;
         return /*#__PURE__*/React.createElement("li", {
           key: id
-        }, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("b", null, lab), " ", /*#__PURE__*/React.createElement("span", null, gender !== null && gender !== void 0 && gender.includes('여성') ? '🙆🏻‍♀️' : '🙆🏻‍♂️'), " ", name));
+        }, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("b", null, lab), " ", /*#__PURE__*/React.createElement("span", null, gender !== null && gender !== void 0 && gender.includes("여성") ? "🙆🏻‍♀️" : "🙆🏻‍♂️"), " ", name));
       })));
     }
   }]);
@@ -97,4 +88,4 @@ function LabButton(props) {
     onClick: props.onFilter
   }, props.children);
 }
-export default likeLionMembers;
+export default LikeLionMembers;
